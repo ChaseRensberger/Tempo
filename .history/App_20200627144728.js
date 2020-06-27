9723f31b0bf782d44897d12e90e58150412090c6
Sara Siye Liu <sara.liu@nyu.edu>
2020-06-27 14:52:58 -0400

    song input

---
+++ .history/App_20200627144728.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style = {{
+                paddingTop: 0,
+                paddingBottom: 4,
+                fontFamily: 'montserrat',
+                fontSize: 16,
+                color: '#5A6067',
+            }}>Profession</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
