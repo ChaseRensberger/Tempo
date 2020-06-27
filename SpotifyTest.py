7c2e7b12729d5925cb02a23b6d9f9a0ec5be0488
ChaseRensberger <chr5119@psu.edu>
2020-06-27 15:58:35 -0400

    add index and analysis template

--- SpotifyTest.py
+++ SpotifyTest.py
@@ -3,20 +3,19 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 CLIENT_ID = '0e92a854dd28455fbb9a1c91ea9787d4'
-CLIENT_ID_SECRET = '2b88d74df8bc4500940b46cb9183e02f'
+CLIENT_ID_SECRET = '2b88d74df8bc4500940b46cb9183e02f' #NEED TO FIGURE OUT HOW TO REMOVE THIS AND GET IT WORKING ON ANY DEVICE
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+
+artist_uri = 'spotify:track:7f8iBUiwio1oX5lAFwC5xI' #Enter any artist URI here
 
 client_credentials_manager = SpotifyClientCredentials(client_id = CLIENT_ID, client_secret = CLIENT_ID_SECRET)
 
 spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+results = spotify.artist_top_tracks(artist_uri)
 
-results = spotify.artist_albums(birdy_uri, album_type = 'album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
